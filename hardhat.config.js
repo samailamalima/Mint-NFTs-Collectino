73eaea3c93b01db6f731f6ada82238c3b7564818
Samaila Anthony Malima <samuelmalima09@gmail.com>
2022-06-22 11:06:43 +0100

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-ethers');
+require("dotenv").config({ path: ".env" });
+
+module.exports = {
+  solidity: '0.8.1',
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
